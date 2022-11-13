5718562cb39a6e705d8f1edc4bd95b5dbccf1e8b
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-11-13 08:50:24 -0700

    finished small visual fixes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const drinkName = document.querySelector('.drink-name')
 const recipeText = document.querySelector('.recipe-text')
 const drinkPic = document.querySelector('.drink-pic')
 const vodka = document.querySelector('#vodka')
@@ -21,16 +20,53 @@ function grabId(data){
 };
 
 function calledFun(data){
-data.forEach(id => {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then(response => response.json())
-    .then(genData => {
-        drinkName.textContent = `${genData['drinks'][0]['strDrink']}`
-        recipeText.textContent = `${genData['drinks'][0]['strInstructions']}`
-        drinkPic.src = `${genData['drinks'][0]['strDrinkThumb']}`
-        drinkName2.textContent = `${genData['drinks'][0]['strDrink']}`
-    })})
+    let resultsList = document.querySelector('.results-wrapper');
+    resultsList.innerHTML = '';
+    data.forEach(id => {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(response => response.json())
+        .then(genData => {
+            console.log(genData)
+            let item = document.createElement('div');
+            let imageContainer = document.createElement('div');
+            let image = document.createElement('img');
+            let drinkName = document.createElement('h3');
+            let recipeButton = document.createElement('a');
+            item.classList.add('results');
+            image.classList.add('drink-pic');
+            drinkName.classList.add('drink-name');
+            recipeButton.id = "recipe-button";
+            imageContainer.classList.add('image-result');
+            image.src = `${genData['drinks'][0]['strDrinkThumb']}`
+            image.classList.add('drink-picture')
+            imageContainer.appendChild(image)
+            item.appendChild(imageContainer);
+            item.appendChild(drinkName)
+            item.appendChild(recipeButton)
+            resultsList.appendChild(item);
+            recipeButton.textContent = "click for recipe"
+            recipeButton.addEventListener('click', e => {
+                e.preventDefault();
+                document.querySelector('.blurred-background').style.display = 'flex';
+                recipeText.textContent = `${genData['drinks'][0]['strInstructions']}`
+                drinkName2.textContent = `${genData['drinks'][0]['strDrink']}`
+            })
+            document.querySelector('.close').addEventListener('click', e => {
+                document.querySelector('.blurred-background').style.display = 'none';
+            })
+            drinkName.textContent = `${genData['drinks'][0]['strDrink']}`
+            console.log(genData['drinks'][0]['strInstructions'])
+        })
+    })
 }
+
+{/* <div class="results">
+    <div class="image-result">
+        <img class="drink-pic" src="https://pngitem.com/pimgs/m/147-1473539_tequila-bottles-png-patron-tequila-silver-png-transparent.png">
+    </div>
+    <h3 class="drink-name">Time to Party</h3>
+    <a id="#recipe-button">click for recipe</a>
+</div> */}
 
 fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Whiskey')
     .then(response => response.json())
@@ -70,11 +106,8 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
             console.log('hello')
             grabId(vodkaData)
         })
-})
-
-document.querySelector('#home').addEventListener('click', e => {
-    alert('HOME: ALREADY ON HOME PAGE')
-})
+    }
+)
 
 document.querySelector('#about-us').addEventListener('click', e => {
     alert('ABOUT-US: PAGE NOT FOUND')
@@ -88,12 +121,12 @@ document.querySelector('#contact').addEventListener('click', e => {
     alert('CONTACT: PAGE NOT FOUND')
 })
 
-document.getElementById('#recipe-button').addEventListener('click', e => {
+document.getElementById('recipe-button').addEventListener('click', e => {
     e.preventDefault();
+    console.log('yes')
     document.querySelector('.blurred-background').style.display = 'flex';
 })
 
 document.querySelector('.close').addEventListener('click', e => {
-    e.preventDefault();
     document.querySelector('.blurred-background').style.display = 'none';
 })
